447ed3a97fef0163fe77646fd43a62ac0a67725d
FreedomWriter <natalienetworks@gmail.com>
2019-11-13 20:21:46 -0600

    ProductContext added and Consumed

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import data from "./data";
 import Navigation from "./components/Navigation";
 import Products from "./components/Products";
 import ShoppingCart from "./components/ShoppingCart";
+import { ProductContext } from "./contexts/ProductContext";
 
 function App() {
   const [products] = useState(data);
@@ -13,23 +14,25 @@ function App() {
 
   const addItem = item => {
     // add the given item to the cart
-    console.log(`App.js: addItem: item: `, item);
+    // console.log(`App.js: addItem: item: `, item);
     setCart(item);
   };
 
-  //   console.log(`App.js: cart: item: `, cart);
+  console.log(`App.js: cart: item: `, cart);
   return (
     <div className="App">
-      <Navigation cart={cart} />
+      <ProductContext.Provider value={{ products, addItem }}>
+        <Navigation cart={cart} />
 
-      {/* Routes */}
-      <Route
-        exact
-        path="/"
-        render={() => <Products products={products} addItem={addItem} />}
-      />
+        {/* Routes */}
+        <Route
+          exact
+          path="/"
+          render={() => <Products products={products} addItem={addItem} />}
+        />
 
-      <Route path="/cart" render={() => <ShoppingCart cart={cart} />} />
+        <Route path="/cart" render={() => <ShoppingCart cart={cart} />} />
+      </ProductContext.Provider>
     </div>
   );
 }
